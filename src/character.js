ec0b087a105e1061ef00ae1d59a90d965fa750fb
Erik Rollwage <126534032+Xenrathe@users.noreply.github.com>
2025-01-13 10:36:54 -0500

    Begin structuring character data storage and retrieval

---
+++ src/character.js
@@ -0,0 +1,50 @@
+export class Character {
+    constructor(abilityScores, name, level, race, raceStat, job, jobStat, jobTalents, feats, lightArmor, hasShield, weaponType, oneUniqueThing, iconRelationships, backgrounds) {
+        this.abilityScores = abilityScores; //[10, 11, 13, 10, 14, 18] = [str, con, dex, int, wis, cha]
+        this.name = name; //a string
+        this.level = level; //a number 1-10
+        this.race = race; //an enum or string
+        this.raceStat = raceStat; //an enum or string
+        this.job = job; //an enum or string
+        this.jobStat = jobStat; //an enum or string
+        this.jobTalents = jobTalents; //an array of #s representing enums,
+        this.feats = feats; //an array of #s representing feat enums
+        this.lightArmor = lightArmor; //true = light armor, false = heavy armor
+        this.hasShield = hasShield; //boolean
+        this.weaponType = weaponType; //a number representing a category
+        this.oneUniqueThing = oneUniqueThing; //a longer string
+        this.iconRelationships = iconRelationships; //an array of objects, like [{'archmage', 1}, {'lich', -2}]
+        this.backgrounds = backgrounds; //an array of objects, like [{'street thief', 4}, {'magician', 2}]
+
+        this.maxHP = this.calculateMaxHP();
+        this.AC = this.calculateAC();
+        this.PD = this.calculatePD();
+        this.MD = this.calculateMD();
+        this.recoveryDice = this.calculateRecoveryDice();
+        this.racialPowers = this.calculateRacialPowers();
+      }
+
+    calculateMaxHP() {
+
+    }
+
+    calculateAC() {
+
+    }
+
+    calculatePD() {
+
+    }
+
+    calculateMD() {
+
+    }
+
+    calculateRecoveryDice() {
+
+    }
+
+    calculateRacialPowers() {
+
+    }
+}
---
+++ src/data/races.js
@@ -0,0 +1,40 @@
+import getRacialPowersAndFeats from './racialpowers';
+
+export const races = {
+  "Dark Elf": {
+    abilityBonus: ["dex", "cha"],
+    racialPowersAndFeats: getRacialPowersAndFeats("Dark Elf")
+  },
+  "Dwarf": {
+    abilityBonus: ["con", "wis"],
+    racialPowersAndFeats: getRacialPowersAndFeats("Dwarf")
+  },
+  "Gnome": {
+    abilityBonus: ["dex", "int"],
+    racialPowersAndFeats: getRacialPowersAndFeats("Gnome")
+  },
+  "Half-elf": {
+    abilityBonus: ["con", "cha"],
+    racialPowersAndFeats: getRacialPowersAndFeats("Half-elf")
+  },
+  "Halfling": {
+    abilityBonus: ["con", "dex"],
+    racialPowersAndFeats: getRacialPowersAndFeats("Halfling")
+  },
+  "Half-orc": {
+    abilityBonus: ["str", "dex"],
+    racialPowersAndFeats: getRacialPowersAndFeats("Half-orc")
+  },
+  "High Elf": {
+    abilityBonus: ["int", "cha"],
+    racialPowersAndFeats: getRacialPowersAndFeats("High Elf")
+  },
+  "Human": {
+    abilityBonus: ["str", "con", "dex", "int", "wis", "cha"],
+    racialPowersAndFeats: getRacialPowersAndFeats("Human")
+  },
+  "Wood Elf": {
+    abilityBonus: ["dex", "wis"],
+    racialPowersAndFeats: getRacialPowersAndFeats("High Elf")
+  },
+};
