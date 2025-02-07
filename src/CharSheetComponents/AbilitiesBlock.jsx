68a698abce3866d49649c4b09c6c44d5de68b067
Erik Rollwage <126534032+Xenrathe@users.noreply.github.com>
2025-02-07 09:55:36 -0500

    Implement (partially) the info buttons for AbilityBlock component

--- src/CharSheetComponents/AbilitiesBlock.jsx
+++ src/CharSheetComponents/AbilitiesBlock.jsx
@@ -4,13 +4,7 @@ import genFeats from "../data/abilities/general.json"
 import { useState } from "react";
 
 function PopupModal({popupInfo, setPopupInfo}) {
-  if (popupInfo.mode == "hidden") {
-    return (
-      <div id="popupMod" className="hidden">
-
-      </div>
-    );
-  } else if (popupInfo.list != null) {
+  if (popupInfo.list != null) {
     return (
       <div id="popupMod" className="visible">
 
@@ -18,46 +12,64 @@ function PopupModal({popupInfo, setPopupInfo}) {
     );
   } else if (popupInfo.singleItem != null) {
     return (
-      <div id="popupMod" className="visible">
-        <button className="close-btn" onClick={() => setPopupInfo({ ...popupInfo, mode: "hidden" })}>✖</button>
+      <div id="popupMod" className={`visible ${popupInfo.singleItem.Base.length > 1000 ? "wide" : ""}`}>
+        <button className="close-btn" onClick={() => setPopupInfo({ title: "", singleItem: null, list: null })}>✖</button>
         <span className="title">{popupInfo.title}</span>
-        <span className="description">{popupInfo.singleItem.Base}</span>
+        <span className="description">
+          {popupInfo.singleItem.Base.split("\n\n").map((paragraph, index) => (
+            <span key={index}>
+              {paragraph}
+            <br /><br />
+        </span>
+    ))}
+  </span>
+      </div>
+    );
+  } else {
+    return (
+      <div id="popupMod" className="hidden">
+
       </div>
     );
   }
 }
 
-function generateLinedInputWithBtn(mode, character) {
+function generateLinedInputWithBtn(mode, character, setPopupInfo) {
   let numLines = 10;
   let dataArray = [];
 
-  //mode = "general"
-  // --includes associated feats from character.feats.talent
   if (mode == "general") {
 
     // includes racialpowers from races[character.race].racialPowersAndFeats
-    Object.keys(races[character.race].racialPowersAndFeats).forEach((title) => {
-      if (title != "Adventurer" && title != "Champion" && title != "Epic") {
-        dataArray.push(title);
+    Object.entries(races[character.race].racialPowersAndFeats).forEach(([title, obj]) => {
+      if (!["Adventurer", "Champion", "Epic"].includes(title)) {
+        dataArray.push({ [title]: obj });
       }
     });
 
     // --includes racialfeats from character.feats.racial
-    Object.keys(character.feats.racial).forEach((title) => {
-        dataArray.push(title);
+    // data structure: {"Adventurer": "Heritage of the Sword"}
+    character.feats.racial.forEach((entry) => {
+      const tier = Object.keys(entry)[0];
+      const title = Object.values(entry)[0];
+      const adjTitle = `(${tier.substring(0,1)}) ${title}`;
+      const obj = {"Base": races[character.race].racialpowersAndFeats[tier][title]};
+      dataArray.push({[title]: obj});
     });
 
-    // includes features from jobs[character.job].features 
-    Object.keys(jobs[character.job].features).forEach((title) => {
-      if (title != "Adventurer" && title != "Champion" && title != "Epic") {
-        dataArray.push(title);
+    // includes features from jobs[character.job].features
+    Object.entries(jobs[character.job].features).forEach(([title, obj]) => {
+      if (!["Adventurer", "Champion", "Epic"].includes(title)) {
+        dataArray.push({ [title]: obj });
       }
     });
 
+    console.log(dataArray);
+    /* 
     // includes feats from the character.feats.general
     Object.keys(character.feats.general).forEach((title) => {
       dataArray.push(title);
-    });
+    });*/
 
   }
 
@@ -76,11 +88,14 @@ function generateLinedInputWithBtn(mode, character) {
   const lines = [];
 
   for (let i = 1; i <= numLines; i++) {
+    const item = dataArray[i - 1];
+    const title = item ? Object.keys(item)[0] : "";
+    const obj = item ? Object.values(item)[0] : "";
+  
     lines.push(
       <div key={`k-${mode}-${i}`} className="single-line-w-btn">
-        <span className="lined-input">{dataArray[i - 1] || ""}</span>
-        {dataArray[i - 1] && <button>i</button>}
-        {!dataArray[i - 1] && <button>+</button>}
+        <span className="lined-input">{title}</span>
+        {item ? <button onClick={() => setPopupInfo({title: title, singleItem: obj, list: null})}>i</button> : <button>+</button>}
       </div>
     );
   }
@@ -91,7 +106,6 @@ function generateLinedInputWithBtn(mode, character) {
 function AbilitiesBlock({ character, abilitiesBlock, setAbilitiesBlock }) {
 
   const [popupInfo, setPopupInfo] = useState({
-    mode: "",
     title: "Cruel",
     singleItem: races["Dark Elf"].racialPowersAndFeats["Cruel"],
     list: null,
@@ -103,19 +117,19 @@ function AbilitiesBlock({ character, abilitiesBlock, setAbilitiesBlock }) {
       <div className="title-label">Abilities</div>
       <div id="job-race-gen" className="abilities-input lined-inputs">
         <label className="subtitle-label">Class, Race, Gen Feats</label>
-        {generateLinedInputWithBtn("general", character)}
+        {generateLinedInputWithBtn("general", character, setPopupInfo)}
       </div>
       <div id="talents" className="abilities-input lined-inputs">
         <label className="subtitle-label">Talents</label>
-        {generateLinedInputWithBtn("talents", character)}
+        {generateLinedInputWithBtn("talents", character, setPopupInfo)}
       </div>
       <div id="spells" className="abilities-input lined-inputs">
         <label className="subtitle-label">Spells</label>
-        {generateLinedInputWithBtn("spells", character)}
+        {generateLinedInputWithBtn("spells", character, setPopupInfo)}
       </div>
       <div id="bonusAbs" className="abilities-input lined-inputs">
         <label className="subtitle-label">Bonus Abs</label>
-        {generateLinedInputWithBtn("bonusAbs", character)}
+        {generateLinedInputWithBtn("bonusAbs", character, setPopupInfo)}
       </div>
     </div>
   );
